9a82e13f90a25ad5f1a3523c9c31972153eebe2d
Nikitakumarisharma <113896209+Nikitakumarisharma@users.noreply.github.com>
2025-03-12 15:34:21 +0530

    xyz

--- src/Pages/AboutPage.jsx
+++ src/Pages/AboutPage.jsx
@@ -3,7 +3,7 @@ import BreadCumb from "../Components/Common/BreadCumb";
 import Counter3 from "../Components/Counter/Counter3";
 import Team2 from "../Components/Team/Team2";
 import Testimonial3 from "../Components/Testimonial/Testimonial3";
-// import Value1 from "../Components/Value/Value1";
+import Value1 from "../Components/Value/Value1";
 
 
 const AboutPage = () => {
@@ -15,7 +15,7 @@ const AboutPage = () => {
             ></BreadCumb>
             <About2 addclass="about-section-2 fix section-padding"></About2>
             <Counter3></Counter3>
-            {/* <Value1></Value1> */}
+            <Value1></Value1>
             <Team2></Team2>
             <Testimonial3></Testimonial3>
         </div>
